049c609564a8401fbcef0a8aad03f7cf2d2f6a52
David Zhang <daqing@mindgee.io>
2024-05-26 18:05:15 +0800

    Finish parsing LET and RETURN statements

---
+++ ast/ast.go
@@ -0,0 +1,27 @@
+package ast
+
+import "monkey/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type ReturnStatement struct {
+	Token       token.Token // the 'return' token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode() {}
+func (rs *ReturnStatement) TokenLiteral() string {
+	return rs.Token.Literal
+}
